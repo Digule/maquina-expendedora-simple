be0a0661fe89995a2e24864a62ff2607d85659e4
Diego <digule@gmail.com>
2023-10-27 13:29:29 +0200

    metodoGetter1

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -69,6 +69,6 @@ public class MaquinaExpendedoraSimple {
         balanceClienteActual = 0;
     }
     public int getTotalDineroAcumulado(){
-        return balanceClienteActual;
+        return totalDineroAcumulado;
     }
 }
